2752d8863348eb4b3996cb3cd633bf15383dcdbc
Juan1511 <j15020611zulu@gmail.com>
2025-06-08 10:38:04 -0500

    fix: The syntax of article Id and category Id in the articleCategory table has been corrected to capture the data correctly

--- migrations/20250522153719-create-article-category.js
+++ migrations/20250522153719-create-article-category.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      ArticleId: {
+      articleId: {
         type: Sequelize.INTEGER,
         references: {
           model: {
@@ -18,7 +18,7 @@ module.exports = {
           }
         },
       },
-      CategoryId: {
+      categoryId: {
         type: Sequelize.INTEGER,
         references: {
           model: {
